d8d89b7dad47220317081fb4a2e264d9deb13cf7
ADHEESHA SOORIYAARACHCHI <106448404+adeshasur@users.noreply.github.com>
2025-12-07 20:37:37 +0530

    Config

---
+++ supermarket-frontend/src/config/api.js
@@ -0,0 +1,9 @@
+const API_BASE_URLS = {
+    PRODUCTS: 'http://localhost:8081',
+    ORDERS: 'http://localhost:8082',
+    INVENTORY: 'http://localhost:8083',
+    USERS: 'http://localhost:8084',
+    PAYMENT: 'http://localhost:8085',
+};
+
+export default API_BASE_URLS;
