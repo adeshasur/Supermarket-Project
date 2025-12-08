e76721933ad1db930e109da400b01bacc2a2351c
Sithum-ek <sithumek@gmail.com>
2025-12-08 11:51:24 +0530

    Update Payment

---
+++ payment-service/src/main/java/com/supermarket/payment_service/Application/controller/PaymentController.java
@@ -0,0 +1,49 @@
+package com.supermarket.payment_service.Application.controller;
+
+
+import com.supermarket.payment_service.Application.dto.request.CreatePaymentDto;
+import com.supermarket.payment_service.Application.dto.response.PaymentGeneralDto;
+import com.supermarket.payment_service.data.entity.Payment;
+import com.supermarket.payment_service.data.service.PaymentService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin(origins = "http://localhost:3000")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Payment> addPayment(@RequestBody CreatePaymentDto createpaymentDto) {
+        return paymentService.addPayment(createpaymentDto);
+    }
+    @GetMapping("/get")
+    public ResponseEntity<PaymentGeneralDto> getPaymentById(@RequestParam Integer id) {
+        return paymentService.getPayment(id);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Payment> deletePaymentById(@RequestParam Integer id) {
+        return paymentService.deletePaymentById(id);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+
+
+
+
+}
